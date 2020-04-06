942a2410f654da9c87abf5a62d5d985a8da67d42
mmendes1 <mmendes1@worcester.edu>
2020-04-06 17:41:29 -0400

    Refactored Code

--- src/baseUi.java
+++ src/baseUi.java
@@ -4,65 +4,85 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class baseUi {
-    private JPanel mainPanel;
-    private JButton itemSet;
-    private JLabel farming;
-    private JTextField itemCurr;
-    private JTextPane item;
-    private JButton inc;
-    private JButton dec;
-    private JTextPane runNum;
-    private JButton reset;
-    private int currRun;
-    private String runLabel = "Run: ";
+    private JPanel     mainPanel;
+    private JButton    itemFieldSet;
+    private JLabel     farmingLabel;
+    private JTextField itemEntryField;
+    private JTextPane  currentItem;
+    private JButton    incrementRunCount;
+    private JButton    decrementRunCount;
+    private JTextPane  runNum;
+    private JButton    resetTextFields;
+    private int        currentRunNumber;
+    private String     runLabel = "Run: ";
 
-    public baseUi(){
-        itemSet.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e){setItemEntryField();}
-        });
-        currRun = 0;
-        inc.addActionListener(new ActionListener() {
+    public baseUi() {
+
+        //All action listeners perform given function on button press, this being ActionEvent e
+        itemFieldSet.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (currRun<0)currRun = 0; else currRun++; runNum.setText(String.valueOf(runLabel + currRun));
+                setItemEntryField();
             }
         });
-        dec.addActionListener(new ActionListener() {
+
+        currentRunNumber = 0;
+
+        incrementRunCount.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (currRun<0)currRun = 0;else currRun--; runNum.setText(String.valueOf(runLabel+currRun)); }
+                if (currentRunNumber < 0) currentRunNumber = 0;
+                else currentRunNumber++;
+                    runNum.setText(String.valueOf(runLabel + currentRunNumber));
+            }
         });
-        reset.addActionListener(new ActionListener() {
+
+        decrementRunCount.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) { item.setText("");
-            itemCurr.setText("");runNum.setText(runLabel); }
+            public void actionPerformed(ActionEvent e) {
+                if (currentRunNumber < 0) currentRunNumber = 0;
+                else currentRunNumber--;
+                    runNum.setText(String.valueOf(runLabel + currentRunNumber));
+            }
+        });
+
+        resetTextFields.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentItem.setText("");
+                itemEntryField.setText("");
+                runNum.setText(runLabel);
+            }
         });
     }
-    public void setItemEntryField ()
-    {
-        if (itemCurr.isVisible())
-        { item.setText(itemCurr.getText());
-        itemCurr.setVisible(false);
-        mainPanel.revalidate();
-        mainPanel.repaint();
 
+    public void setItemEntryField() {
+        if (itemEntryField.isVisible()) {
+            currentItem.setText(itemEntryField.getText());
+            itemEntryField.setVisible(false);
+                mainPanel.revalidate();
+                mainPanel.repaint();
+        } else if (!itemEntryField.isVisible()) {
+            itemEntryField.setVisible(true);
+                mainPanel.revalidate();
+                mainPanel.repaint();
+        } else {
+            currentItem.setText("ERROR IN DISPLAYING ENTRY FIELD");
         }
-        else if (!itemCurr.isVisible())
-        { itemCurr.setVisible(true);
-        mainPanel.revalidate();
-        mainPanel.repaint(); }
-        else
-        { item.setText("ERROR IN DISPLAYING ENTRY FIELD"); }
     }
 
-    public static void main (String[] args) {
-        JFrame frame = new JFrame ("Farming Counter");
-    Dimension appSize = Toolkit.getDefaultToolkit().getScreenSize();
-    frame.setSize(appSize.width/4,appSize.height/4);
-    frame.setContentPane(new baseUi().mainPanel);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
+    private static JFrame createInterface() {
+        JFrame tempFrame = new JFrame("Farming Counter");
+            Dimension appSize = Toolkit.getDefaultToolkit().getScreenSize();
+            tempFrame.setSize(appSize.width / 4, appSize.height / 4);//MAY NOT FIT ALL SCREEN SIZES PROPERLY
+            tempFrame.setContentPane(new baseUi().mainPanel);
+            tempFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return tempFrame;
+    }
+
+    public static void main(String[] args) {
+        JFrame userInterface = createInterface();
+        userInterface.setVisible(true);
     }
 
 }
